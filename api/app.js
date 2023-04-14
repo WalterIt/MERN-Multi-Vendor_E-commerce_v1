47c33c9f7448de3a4e3578046463016c04ea5367
WalterIt <whiruba@gmail.com>
2023-04-14 07:07:14 -0300

    BACKEND => Added cors url

--- api/app.js
+++ api/app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://vs-marketplace.vercel.app"],
+    origin: ["http://localhost:3000", "https://marketplace-7k38.onrender.com"],
     credentials: true,
   })
 );
