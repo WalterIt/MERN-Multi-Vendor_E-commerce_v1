b01dbd92893840b3ce88457aae559536ccbb1519
WalterIt <whiruba@gmail.com>
2023-04-14 10:20:10 -0300

    API=> Upadated cors origin

--- api/app.js
+++ api/app.js
@@ -9,7 +9,11 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://marketplace-7k38.onrender.com"],
+    origin: [
+      "http://localhost:3000",
+      "https://marketplace-7k38.onrender.com",
+      "https://vs-marketplace.vercel.app",
+    ],
     credentials: true,
   })
 );
